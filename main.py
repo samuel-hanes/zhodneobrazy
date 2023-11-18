0557d869b8500c47f2442073791bc3cc11ff150d
samuel-hanes <89020185+samuel-hanes@users.noreply.github.com>
2023-11-18 21:29:38 +0100

    implemented arc unfinished

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from tkinter import *
-from turtle import ScrolledCanvas, RawTurtle, TurtleScreen
+
 import math
-root = Tk()
-canvas = ScrolledCanvas(root)
-canvas.pack(side=LEFT)
+import turtle
+from turtle import RawTurtle, TurtleScreen
+import tkinter as tk
+from tkinter import *
+
+window = tk.Tk()
+window.minsize(width = 1000, height = 800)
+canvas = Canvas(window, width = 1000, height = 800,bd=0)
+canvas.pack()
 
 screen = TurtleScreen(canvas)
-turtle = RawTurtle(canvas)
+screen.setworldcoordinates(0, 800, 1000, 0)
+
+#screen.setworldcoordinates(-180, -90, 180, 90)
+canvas.create_line(0,0,1000,800,fill="red")
+canvas.create_line(1000,0,0,800,fill="red")
+
+turtle = RawTurtle(screen)
+turtle.goto(0,0)
+turtle.goto(1000,800)
 turtle.up()
 
 def cline(a):
@@ -20,30 +33,51 @@ def cline(a):
 def nangler(n,stx,sty,a,typ):
     turtle.goto(stx,sty)
     turtle.down()
-    stxs=100
-    stys=150
-    canvas.create_oval(stx-5,sty-5,stx+5,sty+5)
-    turtle.goto(stx,sty)
+    stxs=300
+    stys=450
+    canvas.create_oval(stxs-5,stys-5,stxs+5,stys+5)
+    
     if typ==1:
         for i in range(n):
             turtle.fd(a)
             turtle.lt(360/n)
-        x=stx-stxs
-        y=sty-stys
-        s=math.atan2(stx-stxs,sty-stys)*180/math.pi #uhol vypocet od x osi
+        for i in range(n):
+            x=stx-stxs
+            y=sty-stys
+            s=math.atan2(abs(sty-stys),abs(stx-stxs))*180/math.pi #uhol vypocet od x osi
+            print(s)
+            if s>0:
+                ang=360-s
+            else:ang=s
+            start=0
+            if stxs>stx and stys>sty:
+                ang=ang*-1
+                start=270
+            elif stys>sty:
+                ang = ang +180
+                start=180
+            elif stxs<stx:
+                ang = ang*-1+180
+                start=90
+            
+            turtle.lt(ang)
+            cline(2*math.sqrt((stx-stxs)**2+(sty-stys)**2))
+            arcx,arcy=turtle.pos()
+            #canvas.create_arc(arcx+10,arcy+10,arcx-(math.sqrt(2)*10-10),arcy-(math.sqrt(2)*10-10),start=start,style=tk.ARC)
+            #canvas.create_rectangle(arcx+10,arcy+10,arcx-(math.sqrt(2)*10-10),arcy-(math.sqrt(2)*10-10))
+            #canvas.create_line(arcx-10,arcy-10,arcx,arcy,arcx+10,arcy+10,smooth=1)
+            canvas.create_arc(arcx-(math.sqrt(2)*a-a),arcy-(math.sqrt(2)*a-a),arcx+(math.sqrt(2)*a+a),arcy+(math.sqrt(2)*a+a),start=start,style=tk.ARC)
+            canvas.create_oval(arcx-1,arcy-1,arcx+1,arcy+1)
+            turtle.goto(stx,sty)
+            turtle.setheading(0)
+            turtle.lt(360/n*i)
+            turtle.fd(a)
+            turtle.setheading(0)
+            stx,sty= turtle.pos()
 
-        if s>0:
-            ang=360-s
-        else:ang=s
-
-
+        print(math.sqrt((stx-stxs)**2+(sty-stys)**2))
         
-        turtle.lt(ang)
-        cline(math.sqrt((stx-stxs)**2+(sty-stys)**2))
-        turtle.goto(stx,sty)
-        print(ang)
-        
 
-nangler(10,100,100,20,1)
+nangler(10,400,400,20,1)
 
-screen.mainloop()
+mainloop()
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import turtle
 from turtle import RawTurtle, TurtleScreen
 import tkinter as tk
 from tkinter import *
-
+import math
 window = tk.Tk()
 window.minsize(width = 720, height = 800)
 canvas = Canvas(window, width = 720, height = 800,bd=0)
@@ -10,10 +10,17 @@ canvas.pack()
 
 screen = TurtleScreen(canvas)
 screen.setworldcoordinates(0, 800, 720, 0)
-
+a=50
+arcx,arcy = 500,500
+canvas.create_arc(arcx-(math.sqrt(2)*a-a),arcy-(math.sqrt(2)*a-a),arcx+(math.sqrt(2)*a+a),arcy+(math.sqrt(2)*a+a),start=90)
+canvas.create_rectangle(arcx+a,arcy+a,arcx-(math.sqrt(2)*a-a),arcy-(math.sqrt(2)*a-a))
+canvas.create_oval(arcx-1,arcy-1,arcx+1,arcy+1)
+canvas.create_rectangle(300,300,400,400)
+canvas.create_arc(300,300,400,400)
 #screen.setworldcoordinates(-180, -90, 180, 90)
 canvas.create_line(0,0,720,800,fill="red")
 canvas.create_line(720,0,0,800,fill="red")
+#canvas.create_arc(200, 100, 100, 200,start=90,style=tk.ARC)
 quake = RawTurtle(screen)
 quake.shape("circle")
 quake.color("red")
